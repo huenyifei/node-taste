cfb57f12a4fb07e28bfb3ce6797ce2293a519bb3
Colin Wu <huenyifei@126.com>
2017-05-27 22:35:54 +0800

    Create taste-connect

---
+++ taste-connect.js
@@ -0,0 +1,7 @@
+/**
+ * Module   : taste-connect
+ * Function :
+ * Project  : node-taste
+ * License  : Copyright © 2017 Shanghai Zhenhua Heavy Industries Co., Ltd. All rights reserved
+ * Author   : Created by Colin Wu on 2017/5/27.
+ */
