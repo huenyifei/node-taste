ad0f50126587abc4a5749d2e1f0b583c4bf8cb23
Colin Wu <huenyifei@126.com>
2017-05-28 21:23:27 +0800

    整理结构

--- taste-connect.js
+++ taste-connect.js
@@ -12,6 +12,7 @@
  */
 var connect = require('connect')
 var router = require('./lib/router.js')
+var restrict = require('./lib/restrict.js')
 
 var routes = {
   GET: {
@@ -29,27 +30,6 @@ var routes = {
     '/user/:id': function(req, res, id){
       res.end('deleted user ' + id);
     }
-  }
-}
-
-function restrict(req, res, next){
-  var authorization = req.headers.authorization;
-  if(!authorization) {
-    res.statusCode = 401
-    res.setHeader('WWW-Authenticate', 'Basic realm="example"')
-    res.end('Access denied')
-    return
-  }
-
-  var parts = authorization.split(' ')
-  var auth = new Buffer(parts[1], 'base64').toString().split(':')
-  var user = auth[0]
-  var pass = auth[1]
-
-  if (user === 'admin' && pass === 'admin'){
-    return next()
-  } else {
-    return next(new Error('Password error!'))
   }
 }
 
