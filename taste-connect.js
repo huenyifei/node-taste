1c72d03c9858ee1865264a284ae892cb02433761
Colin Wu <huenyifei@126.com>
2017-05-28 21:09:49 +0800

    添加认证中间件 添加路由中间件

--- taste-connect.js
+++ taste-connect.js
@@ -5,3 +5,55 @@
  * License  : Copyright © 2017 Shanghai Zhenhua Heavy Industries Co., Ltd. All rights reserved
  * Author   : Created by Colin Wu on 2017/5/27.
  */
+
+/**
+ * Module dependencies
+ * @private
+ */
+var connect = require('connect')
+var router = require('./lib/router.js')
+
+var routes = {
+  GET: {
+    '/': function(req, res){
+      res.end('hello, world!')
+    },
+    '/users': function(req, res){
+      res.end('tobi, loki, ferret');
+    },
+    '/user/:id': function(req, res, id){
+      res.end('user ' + id);
+    }
+  },
+  DELETE: {
+    '/user/:id': function(req, res, id){
+      res.end('deleted user ' + id);
+    }
+  }
+}
+
+function restrict(req, res, next){
+  var authorization = req.headers.authorization;
+  if(!authorization) {
+    res.statusCode = 401
+    res.setHeader('WWW-Authenticate', 'Basic realm="example"')
+    res.end('Access denied')
+    return
+  }
+
+  var parts = authorization.split(' ')
+  var auth = new Buffer(parts[1], 'base64').toString().split(':')
+  var user = auth[0]
+  var pass = auth[1]
+
+  if (user === 'admin' && pass === 'admin'){
+    return next()
+  } else {
+    return next(new Error('Password error!'))
+  }
+}
+
+connect()
+  .use(restrict)
+  .use(router(routes))
+  .listen(3000)
