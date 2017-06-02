7636e8e3faec175bfad964e901f7bec55a952924
wuxinxin <wuxinxin@zpmc.net>
2017-06-02 15:48:19 +0800

    学习logger中间件 学习favicon中间件 学习method_override中间件(解决所有疑问)

--- taste-method_override.js
+++ taste-method_override.js
@@ -15,7 +15,7 @@ function edit (req, res, next) {
   if ('GET' != req.method) return next()
   res.setHeader('Content-Type', 'text/html')
   res.write('<form method="post" enctype="application/x-www-form-urlencoded">')
-  res.write('<input type="hidden" name="_method" value="PUT" />')
+  res.write('<input type="hidden" name="_method" value="put" />')
   res.write('<input type="text" name="user[name]" value="Tobi" />');
   res.write('<button type="submit">Update</button>')
   res.write('</form>')
@@ -23,16 +23,20 @@ function edit (req, res, next) {
 }
 
 function update (req, res, next) {
-  console.log(req.body)
-  if ('PUT' != req.body._method) return next()
-  console.log(req.body)
+  if ('PUT' != req.method) return next()
   res.end('Updated name to ' + req.body.user.name)
 }
 
 var app = connect()
-  .use(logger('dev'))
-  .use(bodyParser.urlencoded())
-  .use(methodOverride('_method'))
+  .use(bodyParser.urlencoded({extended:true}))
+  .use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      var method = req.body._method
+      delete req.body._method
+      return method
+    }
+  }))
   .use(edit)
   .use(update)
 
